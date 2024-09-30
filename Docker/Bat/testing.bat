1d079c0626b6cf1a8263bc44995067356b65e3fb
Haridev1213 <haridev1213@gmail.com>
2024-09-30 11:54:27 +0530

    adding docker related projects

---
+++ Docker/Bat/testing.bat
@@ -0,0 +1,13 @@
+@echo off
+REM Create a new folder named "NewFolder"
+mkdir NewFolder
+
+REM Navigate into the new folder
+cd NewFolder
+
+REM Create a new text file named "example.txt"
+echo This is a sample text file. > example.txt
+
+REM Display a message indicating the process is complete
+echo The text file has been created in the new folder.
+pause
---
+++ Docker/python/addition2numbers.py
@@ -0,0 +1,11 @@
+# Prompt the user to enter the first number
+num1 = int(input("Enter the first number: "))
+
+# Prompt the user to enter the second number
+num2 = int(input("Enter the second number: "))
+
+# Add the two numbers
+sum = num1 + num2
+
+# Display the result
+print("The sum is:", sum)
